c4179fc20ae8290c5b3924107714822699a9e927
TITO\Tito <tito.badilla@hotmail.com>
2019-01-23 17:25:18 -0600

    Service Client

--- OSSANGULAR/src/app/client/client.service.ts
+++ OSSANGULAR/src/app/client/client.service.ts
@@ -1,14 +1,59 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.prod';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { TokenStorage } from '../login/helper/token-storage';
+import { Client } from 'src/model/client.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  private requestMapping = 'client';
-  private url = environment.apiUrl + this.requestMapping;
+  reqHeader:any;
+  tokenCrypt:any
+  URLAPI=environment.apiUrl;
 
-  constructor(private http: HttpClient) { }
+  private getTokenHeader(){
+    this.tokenCrypt=this.token.getToken();
+    if(this.tokenCrypt!=null){
+      this.reqHeader = new HttpHeaders({ 
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.tokenCrypt
+     });
+    }
+    else{
+      this.reqHeader=null;      
+    }
+  }
+
+  constructor(private http: HttpClient,private token:TokenStorage) { }
+
+  public getAllClients(): Observable<Client[]> {
+    this.getTokenHeader();
+    return this.http.get<Client[]>(this.URLAPI+'client', { headers: this.reqHeader });
+  }
+
+  public insertClient(client: Client):Observable<Client> {
+    this.getTokenHeader();
+    return this.http.post<Client>(this.URLAPI+'client/',client, { headers: this.reqHeader } );
+  }
+
+  public updateClient(client: Client):Observable<Client> {
+    this.getTokenHeader();
+    return this.http.put<Client>(this.URLAPI+'client/',client, { headers: this.reqHeader });
+  }
+
+  public deleteClient(clientId: string) {
+    this.getTokenHeader();
+    return this.http.delete<Client>(this.URLAPI+'client/'+clientId, { headers: this.reqHeader });
+  }
+
+  public getByIdClient(clientId: String) {
+    this.getTokenHeader();
+    return this.http.get<Client>(this.URLAPI+'client/'+clientId, { headers: this.reqHeader });
+  }
+
+  
+
 }
