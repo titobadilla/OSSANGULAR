66be3946bd1ddf8a500df2d9e8f99bbcbc10dee7
TITO\Tito <tito.badilla@hotmail.com>
2019-01-24 10:19:35 -0600

    Cambios en login

--- OSSANGULAR/src/app/login/helper/token-storage.ts
+++ OSSANGULAR/src/app/login/helper/token-storage.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { EventEmitterLogoutService } from '../event-emitter-logout.service';
 
 
 const TOKEN_KEY = 'userToken';
@@ -6,11 +7,18 @@ const TOKEN_KEY = 'userToken';
 @Injectable()
 export class TokenStorage {
   token:any;
-    constructor() { }
+    constructor(private emitterService:EventEmitterLogoutService) { }
 
   signOut() {
     window.localStorage.removeItem(TOKEN_KEY);
     window.localStorage.clear();
+    this.emitterService.setSessionClosedByUser(true);
+  }
+
+  signOutSystem() {
+    window.localStorage.removeItem(TOKEN_KEY);
+    window.localStorage.clear();
+    this.emitterService.setSessionClosedBySystem(true);
   }
 
   public saveToken(token: string) {
