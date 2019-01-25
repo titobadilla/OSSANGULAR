72ed212065bacf1dd8b95534b88b15b6a0ad4a89
DESKTOP-FVM8UDG\Diana Bonilla <dianapamela3197@gmail.com>
2019-01-25 11:49:57 -0600

    interfaz parcial

--- OSSANGULAR/src/app/work-order/work-order.service.ts
+++ OSSANGULAR/src/app/work-order/work-order.service.ts
@@ -1,14 +1,37 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.prod';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { TokenStorage } from '../login/helper/token-storage';
+import { WorkOrder } from 'src/model/workorder.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkOrderService {
 
-  private requestMapping = 'workorder';
-  private url = environment.apiUrl + this.requestMapping;
+  reqHeader:any;
+  tokenCrypt:any
+  URLAPI=environment.apiUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,private token:TokenStorage) { }
+
+  private getTokenHeader(){
+    this.tokenCrypt=this.token.getToken();
+    if(this.tokenCrypt!=null){
+      this.reqHeader = new HttpHeaders({ 
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.tokenCrypt
+     });
+    }
+    else{
+      this.reqHeader=null;      
+    }
+  }
+
+
+  public insertWorkOrder(workOrder: WorkOrder):Observable<WorkOrder>{
+    this.getTokenHeader();
+      return this.http.post<WorkOrder>(this.URLAPI+'workorder/addWorkOrder', workOrder, { headers: this.reqHeader } );
+  }
 }
