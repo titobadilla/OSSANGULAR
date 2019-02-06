f3cde419972e48de3acbfe56731ca23be8a07b2c
DESKTOP-FVM8UDG\Diana Bonilla <dianapamela3197@gmail.com>
2019-02-06 14:48:59 -0600

    interfaz insertar rol de empleado

--- OSSANGULAR/src/app/employee-role/employee-role.component.ts
+++ OSSANGULAR/src/app/employee-role/employee-role.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeRoleService } from './employee-role.service';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators, FormsModule, AbstractControl } from '@angular/forms';
+import { FormValidators } from '@syncfusion/ej2-angular-inputs';
 
 @Component({
   selector: 'app-employee-role',
@@ -9,9 +11,35 @@ import { Router } from '@angular/router';
 })
 export class EmployeeRoleComponent implements OnInit {
 
-  constructor(private router: Router,private employeeRoleService:EmployeeRoleService) { }
+  reactForm: FormGroup;
+
+  constructor(private router: Router,private employeeRoleService:EmployeeRoleService) {
+    this.reactForm = new FormGroup({
+      'name': new FormControl('', [FormValidators.required]),
+      'type': new FormControl('', [FormValidators.required]),
+    });
+   }
 
   ngOnInit() {
+    let formId: HTMLElement = <HTMLElement>document.getElementById('formId');
+    document.getElementById('formId').addEventListener(
+      'submit',
+      (e: Event) => {
+        e.preventDefault();
+        if (this.reactForm.valid) {
+          alert('Customer details added!');
+          this.reactForm.reset();
+        } else {
+          // validating whole form
+          Object.keys(this.reactForm.controls).forEach(field => {
+            const control = this.reactForm.get(field);
+            control.markAsTouched({ onlySelf: true });
+          });
+        }
+      });
   }
+
+  get name() { return this.reactForm.get('name'); }
+  get type() { return this.reactForm.get('type'); }
 
 }
