be3c03c389009f183d745c1a32991e940f0f67f7
TITO\Tito <tito.badilla@hotmail.com>
2019-02-27 15:12:24 -0600

    Cambios en Update Client

--- OSSANGULAR/src/app/client/update-client/update-client.component.ts
+++ OSSANGULAR/src/app/client/update-client/update-client.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-update-client',
+  selector: 'update-client',
   templateUrl: './update-client.component.html',
   styleUrls: ['./update-client.component.css']
 })
 export class UpdateClientComponent implements OnInit {
+
+  @Input() clientId: String;
 
   constructor() { }
 
