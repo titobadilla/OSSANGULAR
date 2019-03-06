b9daff9cf7cd861d09958aec96b0a70300e36e54
TITO\Tito <tito.badilla@hotmail.com>
2019-03-05 21:41:24 -0600

    Chocando

--- OSSANGULAR/src/model/groupclient.model.ts
+++ OSSANGULAR/src/model/groupclient.model.ts
@@ -7,11 +7,8 @@ export class GroupClient{
     contactLastName:String;
     email:String;
     phone1:String;
-<<<<<<< HEAD
     phone2:String;    
-=======
-    phone2:String;
->>>>>>> 93c7bd563ab0110aad6a0ad91e3a1ab941404078
+
     clients:Client[]=new Array();
     
 }
