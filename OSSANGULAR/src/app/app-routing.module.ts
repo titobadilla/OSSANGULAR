459664e6d500d15e8674a5cf9d74022fe691ac54
TITO\Tito <tito.badilla@hotmail.com>
2019-03-30 14:15:19 -0600

    Cambios Routing

--- OSSANGULAR/src/app/app-routing.module.ts
+++ OSSANGULAR/src/app/app-routing.module.ts
@@ -17,25 +17,14 @@ import { TelephoneEmployeeComponent } from './telephone-employee/telephone-emplo
 import { WorkOrderComponent } from './work-order/work-order.component';
 import { WorkOrderDetailComponent } from './work-order-detail/work-order-detail.component';
 import { WorkOrderTypeComponent } from './work-order-type/work-order-type.component';
-import { LoginComponent } from './login/login.component';
 import { CalendarComponent } from './shared/calendar/calendar.component';
 import { AuthGuard } from './login/guards/auth.guard';
-import { AppComponent } from './app.component';
-import { UpdateWorkOrderComponent } from './work-order/update-work-order/update-work-order.component';
-import { InsertEmployeeComponent } from './employee/insert-employee/insert-employee.component';
 import {HttpClientModule} from '@angular/common/http';
 import { ReactiveFormsModule} from '@angular/forms';
-import { InsertWorkOrderDetailComponent } from './work-order-detail/insert-work-order-detail/insert-work-order-detail.component';
-import { UpdateWorkOrderDetailComponent } from './work-order-detail/update-work-order-detail/update-work-order-detail.component';
-
-import { InsertClientComponent } from './client/insert-client/insert-client.component';
 import { InventoryOutputComponent } from './inventory-output/inventory-output.component';
 import { InventoryOutputGeneralComponent } from './inventory-output/inventory-output-general/inventory-output-general.component';
-import { DeleteComponent } from './delete/delete.component';
 import { ToolComponent } from './tool/tool.component';
 import { MaterialComponent } from './material/material.component';
-import { InsertInventoryWorkOrderComponent } from './work-order/insert-inventory-work-order/insert-inventory-work-order.component';
-import { SeeWorkOrderDetailComponent } from './work-order-detail/see-work-order-detail/see-work-order-detail.component';
 
 const routes: Routes = [
   {path:"address",component:AddressComponent, canActivate: [AuthGuard]},
@@ -55,33 +44,15 @@ const routes: Routes = [
   {path:"work-order",component:WorkOrderComponent},
   {path:"work-order-detail",component:WorkOrderDetailComponent, canActivate: [AuthGuard]},
   {path:"work-order-type",component:WorkOrderTypeComponent, canActivate: [AuthGuard]},
-  /*{path:"login",component:LoginComponent},*/
   {path:"calendar",component:CalendarComponent, canActivate: [AuthGuard]},
-  {path:"calendar",component:CalendarComponent},
-  {path:"update-work-order",component:UpdateWorkOrderComponent},
   {path:'work-order-detail',component:WorkOrderDetailComponent},
-  {path:'insert-work-order-detail',component:InsertWorkOrderDetailComponent},
-  {path:'update-work-order-detail',component:UpdateWorkOrderDetailComponent},
-  {path:'see-work-order-detail',component:SeeWorkOrderDetailComponent},
   {path:'work-order-type',component:WorkOrderTypeComponent},
-  {path:'insert-client',component:InsertClientComponent},
   {path:'inventory-output',component:InventoryOutputComponent},
   {path:'inventory-output-general',component:InventoryOutputGeneralComponent},
-  {path:'delete',component:DeleteComponent},
   {path:'tool',component:ToolComponent},
   {path:'brand',component:BrandComponent},
   {path:'device-state',component:DeviceStateComponent},
-  {path:'material',component:MaterialComponent},
-  {path:'insert-work-order',component:InsertInventoryWorkOrderComponent}
-  /*,
-
-  {path:'measurement-unit',component:MeasurementUnitComponent},
-  {path:'tool',component:ToolComponent},
-  {path:'device',component:DeviceComponent},
-  
-  {path:'menu',component:MenuPrincipalComponent}
-    /*,
-  { path: '**', component: PageNotFoundComponent }*/
+  {path:'material',component:MaterialComponent}
 ];
 
 @NgModule({
